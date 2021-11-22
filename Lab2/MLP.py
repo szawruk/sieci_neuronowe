fc342330dbe213514f70c11f97eb4ce71f6b03f2
szawruk <szawruk@gmail.com>
2021-11-22 12:28:09 +0100

    done stage 2 list 2

--- Lab2/MLP.py
+++ Lab2/MLP.py
@@ -1,24 +1,36 @@
 import random
+import time
 
 import numpy as np
 import math
 
 
 class MLP:
-    def __init__(self, input_data_set, input_labels):
+    def __init__(self, input_data_set, input_labels, train_data_set, train_labels, epochs=1000, l_rate=0.1, batch_size=0):
         self.number_of_parameters = len(input_data_set[0])
 
         self.hidden_layers_list = []
         self.layers_activation_functions = []
 
-        self.input_data_set = input_data_set / 255
+        self.input_data_set = input_data_set
         self.input_labels = input_labels
+
+        self.train_data_set = train_data_set
+        self.train_labels = train_labels
 
         self.matrices_with_weights = []
         self.biases = []
 
         self.mean_centre_of_distribution = 0
-        self.standard_deviation = 1
+        self.standard_deviation = 0.1
+
+        self.a_values = {}
+        self.z_values = {}
+
+        self.epochs = epochs
+
+        self.l_rate = l_rate
+        self.batch_size = batch_size
 
     def calculate_weights_matrixes(self):
         for x in range(len(self.hidden_layers_list) + 1):
@@ -44,45 +56,131 @@ class MLP:
         self.calculate_weights_matrixes()
         self.layers_activation_functions.append('smax')
 
-    def calculate_output(self):
-        actual_values_vector = self.input_data_set[0]
+    def forward(self, x_train):
+        self.a_values[0] = x_train
 
         for idx in range(len(self.get_layers_list()) - 1):
-            print('-------------------------')
-            print('Funkcja aktywacji: ', end='')
-            print(self.layers_activation_functions[idx])
+            # print('-------------------------')
+            # print('Funkcja aktywacji: ', end='')
+            # print(self.layers_activation_functions[idx])
 
-            z = (self.matrices_with_weights[idx].dot(actual_values_vector)) + self.biases[idx]
+            z = (self.matrices_with_weights[idx].dot(self.a_values[idx])) + self.biases[idx]
+            self.z_values[idx + 1] = z
             if self.layers_activation_functions[idx] == 'sigm':
-                actual_values_vector = self.sigm(z)
+                self.a_values[idx + 1] = self.sigm(z)
             if self.layers_activation_functions[idx] == 'tanh':
-                actual_values_vector = self.tanh(z)
+                self.a_values[idx + 1] = self.tanh(z)
             if self.layers_activation_functions[idx] == 'relu':
-                actual_values_vector = self.relu(z)
+                self.a_values[idx + 1] = self.relu(z)
             if self.layers_activation_functions[idx] == 'smax':
-                actual_values_vector = self.softmax(z)
+                self.a_values[idx + 1] = self.softmax(z)
 
-            print('Wyjście z warstwy: ', end='')
-            print(actual_values_vector)
+            # print('Wyjście z warstwy: ', end='')
+            # print(self.a_values[idx + 1])
+        # print(self.a_values)
+        return self.a_values[len(self.get_layers_list()) - 1]
 
-    def sigm(self, x):
-        return x / (1 + math.e ** (-1))
+    def backward(self, y_train, output):
+        change_w = {}
+        change_b = {}
+        start_idx = len(self.get_layers_list()) - 1
 
-    def tanh(self, x):
-        return (2 / (1 + math.e ** (-2 * x))) - 1
+        error = self.l_rate * (output - y_train) * self.softmax(self.z_values[start_idx], derivative=True)
+        change_w[start_idx] = np.outer(error, self.a_values[start_idx - 1])
+        change_b[start_idx] = error
 
-    def relu(self, x):
-        for i in range(len(x)):
-            if x[i] < 0:
-                x[i] = 0
-        return x
+        for idx in range(len(self.get_layers_list()) - 2, 0, -1):
+            if self.layers_activation_functions[idx - 1] == 'sigm':
+                error = np.dot(self.matrices_with_weights[idx].T, error) * self.sigm(self.z_values[idx], derivative=True)
+            if self.layers_activation_functions[idx - 1] == 'tanh':
+                error = np.dot(self.matrices_with_weights[idx].T, error) * self.tanh(self.z_values[idx], derivative=True)
+            if self.layers_activation_functions[idx - 1] == 'relu':
+                error = np.dot(self.matrices_with_weights[idx].T, error) * self.relu(self.z_values[idx], derivative=True)
 
-    def softmax(self, input_vector):
-        exp_sum = 0
-        for i in input_vector:
-            exp_sum += math.exp(i)
+            change_w[idx] = np.outer(error, self.a_values[idx - 1])
+            change_b[idx] = error
+        # print(change_b)
+        return change_w, change_b
 
-        for i in range(len(input_vector)):
-            input_vector[i] = math.exp(input_vector[i]) / exp_sum
+    def train(self):
+        start_time = time.time()
+        for i in range(self.epochs):
 
-        return input_vector
+            batch_iterator = 0
+            batch_change_w = []
+            batch_change_b = []
+            for x, y in zip(self.input_data_set, self.input_labels):
+                output = self.forward(x)
+                y_labels = np.zeros(len(output))
+                y_labels[y] = 1
+                change_w, change_b = self.backward(y_labels, output)
+                if self.batch_size > 0:
+                    if batch_iterator < self.batch_size:
+                        batch_iterator += 1
+                        batch_change_w.append(change_w)
+                        batch_change_b.append(change_b)
+                    else:
+                        calculated_weights_change = batch_change_w[0]
+                        calculated_biases_change = batch_change_b[0]
+                        for change_idx in range(1, len(batch_change_w)):
+                            calculated_weights_change = {key: np.add(calculated_weights_change[key], batch_change_w[change_idx][key]) for key in batch_change_w[change_idx].keys()}
+                            calculated_biases_change = {key: np.add(calculated_biases_change[key], batch_change_b[change_idx][key]) for key in batch_change_b[change_idx].keys()}
+                        calculated_weights_change = {key: calculated_weights_change[key] / len(batch_change_w) for key in calculated_weights_change.keys()}
+                        calculated_biases_change = {key: calculated_biases_change[key] / len(batch_change_w) for key in calculated_biases_change.keys()}
+
+                        self.update_weights(calculated_weights_change)
+                        self.update_biases(calculated_biases_change)
+
+                        batch_iterator = 0
+                        batch_change_w = []
+                        batch_change_b = []
+
+                else:
+                    self.update_weights(change_w)
+                    self.update_biases(change_b)
+
+            accuracy = self.test_accuracy(self.train_data_set, self.train_labels)
+            print('Epoch: {0}, Time Spent: {1:.2f}s, Accuracy: {2}'.format(
+                i + 1, time.time() - start_time, accuracy
+            ))
+
+    def update_weights(self, change_w):
+        for i in range(len(self.hidden_layers_list) + 1):
+            # self.matrices_with_weights[i] -= self.l_rate * change_w[i + 1]
+            self.matrices_with_weights[i] -= change_w[i + 1]
+
+    def update_biases(self, change_b):
+        for i in range(len(self.hidden_layers_list) + 1):
+            # self.biases[i] -= self.l_rate * change_b[i + 1]
+            self.biases[i] -= change_b[i + 1]
+
+    def test_accuracy(self, x_val, y_val):
+        predictions = []
+
+        for x, y in zip(x_val, y_val):
+            output = self.forward(x)
+            pred = np.argmax(output)
+            predictions.append(pred == y)
+        # print(np.mean(predictions))
+        return np.mean(predictions)
+
+    def sigm(self, x, derivative=False):
+        if derivative:
+            return (np.exp(-x))/((np.exp(-x)+1)**2)
+        return 1/(1 + np.exp(-x))
+
+    def tanh(self, x, derivative=False):
+        if derivative:
+            return 1 - np.tanh(x)**2
+        return np.tanh(x)
+
+    def relu(self, x, derivative=False):
+        if derivative:
+            return np.maximum(np.minimum(x, 1), 0)
+        return np.maximum(0, x)
+
+    def softmax(self, x, derivative=False):
+        exps = np.exp(x - x.max())
+        if derivative:
+            return exps / np.sum(exps, axis=0) * (1 - exps / np.sum(exps, axis=0))
+        return exps / np.sum(exps, axis=0)
--- Lab2/main.py
+++ Lab2/main.py
@@ -1,16 +1,26 @@
 from keras.datasets import mnist
-from MLP import MLP
 
+import numpy as np
 # Press the green button in the gutter to run the script.
+from Lab2.MLP import MLP
+
 if __name__ == '__main__':
     (train_X, train_y), (test_X, test_y) = mnist.load_data()
-    train_X = train_X.reshape((train_X.shape[0], 28 * 28))
+    train_X = train_X.reshape((train_X.shape[0], 28 * 28)) / 255
+    test_X = test_X.reshape((test_X.shape[0], 28 * 28)) / 255
+
     # print('X_train: ' + str(train_X.shape))
 
-    mlp = MLP(train_X, train_y)
-    mlp.add_layer(20, 'sigm')
-    mlp.add_layer(18, 'sigm')
-    mlp.add_layer(14, 'relu')
+    mlp = MLP(train_X[0:2000], train_y[0:2000], test_X[0:1000], test_y[0:1000], epochs=1000, l_rate=0.1, batch_size=0)
+    mlp.add_layer(64, 'sigm')
+    mlp.add_layer(32, 'sigm')
+    # mlp.add_layer(16, 'relu')
     mlp.finalize_model()
-    mlp.calculate_output()
+    mlp.train()
+    # out = mlp.forward(mlp.input_data_set[0])
+    # mlp.backward(mlp.input_labels[0], out)
+
+
+
+
 
